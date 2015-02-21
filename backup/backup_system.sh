d45af57f66735c7170727e602410756ff3b4008c
Pierrick Bouvier <bouvier.pierrick@yahoo.fr>
2015-02-20 21:47:34 -0500

    use clean instead of autoclean

--- backup/backup_system.sh
+++ backup/backup_system.sh
@@ -2,7 +2,7 @@
 echo "Close all programs and press Enter"
 read
 rm -rf /home/user/.cache /home/user/.thumbnails /home/user/.macromedia /tmp
-sudo apt-get autoclean --yes
+sudo apt-get clean
 dpkg --get-selections | grep -v "deinstall$"  > /data/backup/system/dpkg.list
 sudo tar -cvpf /data/backup/system/boot.tar --one-file-system /boot
 sudo tar -cvpf /data/backup/system/system.tar --one-file-system /
