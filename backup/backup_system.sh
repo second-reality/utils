506d6a28831b31fe8efd757c12d5bfb9ba96b8f1
Pierrick Bouvier <bouvier.pierrick@yahoo.fr>
2015-03-07 09:46:27 -0500

    verbose system backup

--- backup/backup_system.sh
+++ backup/backup_system.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo "Close all programs and press Enter"
+echo "--- Close all programs and press Enter"
 read
+echo "--- clean files"
 rm -rf /home/user/.cache /home/user/.thumbnails /home/user/.macromedia /tmp
+echo "--- clean apt"
 sudo apt-get clean
+echo "--- backup packages list"
 dpkg --get-selections | grep -v "deinstall$"  > /data/backup/system/dpkg.list
+echo "--- backup boot"
 sudo tar -cvpf /data/backup/system/boot.tar --one-file-system /boot
+echo "--- backup system"
 sudo tar -cvpf /data/backup/system/system.tar --exclude=/data/ --one-file-system /
+echo "--- sync fs"
 sync
+echo "--- done"
