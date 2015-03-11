edfe3571d95848883f17bedb60b87c7b000dae93
Pierrick Bouvier <bouvier.pierrick@yahoo.fr>
2015-03-11 23:06:56 +0100

    only backup user data

--- backup/backup_system.sh
+++ backup/backup_system.sh
@@ -2,15 +2,13 @@
 echo "--- Close all programs and press Enter"
 read
 echo "--- clean files"
-rm -rf /home/user/.cache /home/user/.thumbnails /home/user/.macromedia /tmp
-echo "--- clean apt"
-sudo apt-get clean
+rm -rf /home/user/.cache /home/user/.thumbnails /home/user/.macromedia
+echo "--- create backup dir"
+mkdir -p /data/backup/system
 echo "--- backup packages list"
 dpkg --get-selections | grep -v "deinstall$"  > /data/backup/system/dpkg.list
-echo "--- backup boot"
-sudo tar -cvpf /data/backup/system/boot.tar --one-file-system /boot
-echo "--- backup system"
-sudo tar -cvpf /data/backup/system/system.tar --exclude=/data/ --one-file-system /
+echo "--- backup user data"
+tar -cvpf /data/backup/system/user.tar /home/user
 echo "--- sync fs"
 sync
 echo "--- done"
