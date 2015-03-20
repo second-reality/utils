28eb4eab58b0971610df21bd7c58edb7e6331533
Pierrick Bouvier <bouvier.pierrick@yahoo.fr>
2015-03-20 15:34:46 +0100

    better backup

--- backup/backup_system.sh
+++ backup/backup_system.sh
@@ -2,13 +2,13 @@
 echo "--- Close all programs and press Enter"
 read
 echo "--- clean files"
-rm -rf /home/user/.cache /home/user/.thumbnails /home/user/.macromedia
+rm -rf /home/user/.cache /home/user/.thumbnails /home/user/.macromedia /home/user/.local/share/Trash/
 echo "--- create backup dir"
 mkdir -p /data/backup/system
 echo "--- backup packages list"
 dpkg --get-selections | grep -v "deinstall$"  > /data/backup/system/dpkg.list
-echo "--- backup user data"
-tar -cvpf /data/backup/system/user.tar /home/user
+echo "--- backup user data and etc"
+sudo tar -cvpf /data/backup/system/system.tar /etc /home/user
 echo "--- sync fs"
 sync
 echo "--- done"
