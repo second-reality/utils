253de3bd2a469541ae39f691fe097d07765c4a06
Pierrick Bouvier <bouvier.pierrick@yahoo.fr>
2016-11-07 07:51:07 +0100

    new make_gallery

--- bin/make_gallery.sh
+++ bin/make_gallery.sh
@@ -6,7 +6,7 @@ die()
   exit 1
 }
 
-pictures_dir="/var/www/public/pictures/"
+pictures_dir="/var/www/pictures/"
 
 [ $# -ne 2 ] && die "usage: src dest"
 src="$1"
@@ -15,11 +15,11 @@ dest="$pictures_dir/$2"
 [ -d "$dest" ] && die "$dest exists"
 mkdir -p "$dest"
 
-rsync -av /var/www/www/PhotoFloat/web/ "$dest/" --exclude cache/ ||
+rsync -av /var/www/pictures/PhotoFloat/web/ "$dest/" --exclude cache/ ||
   die "copy gallery failed"
 mkdir "$dest/cache"
 
-/var/www/www/PhotoFloat/scanner/main.py "$src" "$dest"/cache ||
+/var/www/pictures/PhotoFloat/scanner/main.py "$src" "$dest"/cache ||
   die "gen gallery failed"
 
 echo "gallery available at $dest"
