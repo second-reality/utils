21c07ec5d5f1d2526b0c6d7027b1312d07755e30
pik <nomail@pik.com>
2019-05-11 22:00:54 +0200

    fix testbw

--- bin/testbandwith.sh
+++ bin/testbandwith.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.17.4.tar.gz -O /dev/null
+url=https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.1.tar.xz
+rm -rf /tmp/testbw*
+aria2c -s 10 -x 10 -k 5M -d /tmp -o testbw $url
