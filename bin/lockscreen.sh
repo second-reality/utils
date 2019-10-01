5190212b2072e327b58bce87a0e6c496a88932d7
pik <nomail@pik.com>
2019-10-01 22:06:02 +0200

    lock with blur screen

--- bin/lockscreen.sh
+++ bin/lockscreen.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
 
-i3lock -e -u -c '#ffa500'
+tmp=/tmp/_lockscreen.png
+tmp_blur="$tmp"blur.png
+rm -f $tmp $tmp_blur
+scrot $tmp
+convert -blur 50x15 -fill red -pointsize 50 -gravity center -annotate 0 'LOCKED' $tmp $tmp_blur
+#i3lock -e -u -c '#ffa500'
+i3lock -e -u -i $tmp_blur&
+sleep 1 
+xset dpms force off
+wait
