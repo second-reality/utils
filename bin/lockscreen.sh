fb18884a6a68698d133e1ffa7bf3405fc86fc79a
Pierrick Bouvier <pierrick.bouvier@allegrodvt.com>
2019-10-10 08:28:02 +0200

    better lockscreen

--- bin/lockscreen.sh
+++ bin/lockscreen.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
+
+function screen_off()
+{
+  while [ 1 ]
+  do
+    sleep 10
+    xset dpms force off
+  done
+}
 
 tmp=/tmp/_lockscreen.png
 tmp_blur="$tmp"blur.png
 rm -f $tmp $tmp_blur
 scrot $tmp
-convert -blur 50x15 -fill red -pointsize 50 -gravity center -annotate 0 'LOCKED' $tmp $tmp_blur
+str="LOCKED"
+convert -blur 50x15 -fill red -pointsize 80 -gravity center -annotate 0 "$str" $tmp $tmp_blur
 #i3lock -e -u -c '#ffa500'
-i3lock -e -u -i $tmp_blur&
-sleep 1 
-xset dpms force off
-wait
+i3lock -e -u -n -i $tmp_blur&
+lock_pid=$!
+(screen_off)&
+off_pid=$!
+
+wait $lock_pid
+kill $off_pid
