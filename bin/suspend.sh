07e0950563b8d264057cb3e9de82e42b2f31c2fc
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2020-11-25 00:08:57 +0100

    when suspending, stop all vm running

--- bin/suspend.sh
+++ bin/suspend.sh
@@ -2,5 +2,17 @@
 set -e
 
 #$(dirname $(readlink -f $0))/lockscreen.sh&
+
+# stop all vm
+get_vm()
+{
+  sudo virsh list --state-running --name
+}
+
+while [ "$(get_vm)" != "" ]; do
+  sudo virsh shutdown $(get_vm)
+  sleep 1
+done
+
 sleep 5
 sudo -n /usr/sbin/pm-suspend
