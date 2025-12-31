bf5ebeb677ca740387e42a91aaa8782130ab3a52
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-12-31 11:26:40 -0800

    up

--- bin/suspend.sh
+++ bin/suspend.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
-$(dirname $(readlink -f $0))/lockscreen.sh&
-
 # stop all vm
 get_vm()
 {
   sudo virsh list --state-running --name
 }
+
+if [ "$(get_vm)" != "" ]; then
+    zenity --info --text "VM RUNNING, no suspend" || true
+    exit 1
+fi
+
+$(dirname $(readlink -f $0))/lockscreen.sh&
 
 while [ "$(get_vm)" != "" ]; do
   sudo virsh shutdown $(get_vm)
