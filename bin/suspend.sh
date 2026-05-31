42f149bdc651ce449c8ad134d7ce7c2babe3d839
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2026-05-31 14:57:06 -0700

    suspend

--- bin/suspend.sh
+++ bin/suspend.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 set -e
 
-# stop all vm
-get_vm()
-{
-  sudo virsh list --state-running --name
-}
+vm=win10_gaming
+if [ "$(virsh --connect qemu:///system domstate $vm)" == "running" ]
+then
+    zenity --info --text "VM RUNNING, no suspend" || true
+    exit 1
+fi
 
 $(dirname $(readlink -f $0))/lockscreen.sh&
 
-sudo virsh dompmsuspend win10_gaming mem
-
-#if [ "$(get_vm)" != "" ]; then
-#    zenity --info --text "VM RUNNING, no suspend" || true
-#    exit 1
-#fi
-
-#while [ "$(get_vm)" != "" ]; do
-#  sudo virsh shutdown $(get_vm)
-#  sleep 1
-#done
-
-sleep 10
+sleep 2
 systemctl suspend
 setxkbmap us qwerty-fr
