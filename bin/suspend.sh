7c7372131616659f8ad8b26ade1f08db786705ef
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2026-07-14 21:32:42 -0700

    suspend

--- bin/suspend.sh
+++ bin/suspend.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-$(dirname $(readlink -f $0))/lockscreen.sh&
+vm=win10_gaming
+resume_vm=0
+if [ "$(virsh --connect qemu:///system domstate $vm)" == "running" ]
+then
+    #virsh --connect qemu:///system dompmsuspend $vm mem
+    zenity --info --text "VM RUNNING, no suspend"
+    exit 1
+    #resume_vm=1
+    #sleep 5
+fi
 
-#vm=win10_gaming
-#resume_vm=0
-#if [ "$(virsh --connect qemu:///system domstate $vm)" == "running" ]
-#then
-#    virsh --connect qemu:///system dompmsuspend $vm mem
-#    #zenity --info --text "VM RUNNING, no suspend" || true
-#    resume_vm=1
-#    sleep 5
-#fi
+$(dirname $(readlink -f $0))/lockscreen.sh&
 
 sleep 1
 systemctl suspend
@@ -26,7 +27,7 @@ screen_on()
 }
 
 screen_on&
-sleep 10
+#sleep 10
 #sleep 1
 #if [ "$resume_vm" -eq 1 ]; then
 #    virsh --connect qemu:///system dompmwakeup $vm
